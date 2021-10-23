195c59345d7f523c7b2d03a96378d312daea1acf
wschadow <wolfgang.schadow@t-online.de>
2021-10-23 15:32:03 +0200

    files are only sorted when all configurations are generated

--- CC-build.sh
+++ CC-build.sh
@@ -305,7 +305,7 @@ echo '... done'
 echo
 echo 'generating configurations and macros ....'
 echo
-BUILDPATH=$SCRIPT_PATH/../CB/CC-Duet2WiFi-$CC-Build$BUILD
+BUILDPATH=$SCRIPT_PATH/../CC-build/CC-Duet2WiFi-$CC-Build$BUILD
 # Prepare config files folders
 if [ ! -d "$BUILDPATH" ]; then
     mkdir -p $BUILDPATH || exit 27
@@ -495,15 +495,15 @@ do
 done
 
 # Sort configuration only when build ALL is selected
-#if [ ! -z "$ALL_VARIANTS" ]; then
-#    if [ "$ALL_VARIANTS" == "All" ]; then
+if [ ! -z "$ALL_VARIANTS" ]; then
+    if [ "$ALL_VARIANTS" == "All" ]; then
         $SCRIPT_PATH/sort.sh $BUILDPATH $BUILDPATH-sorted/
-#    else
-#        echo "$(tput setaf 1)ALL_VARIANTS argument is wrong!$(tput sgr0)"
-#        echo "Only $(tput setaf 2)'All'$(tput sgr0) is allowed as argument!$(tput sgr0)"
-#        exit 37
-#    fi
-#fi
+    else
+        echo "$(tput setaf 1)ALL_VARIANTS argument is wrong!$(tput sgr0)"
+        echo "Only $(tput setaf 2)'All'$(tput sgr0) is allowed as argument!$(tput sgr0)"
+        exit 37
+    fi
+fi
 
 
 # housekeeping: delete filament folders in source directory
