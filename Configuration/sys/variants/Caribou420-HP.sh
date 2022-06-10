e2ac58b15a55372c7570ceef39d733f643ac2a86
wschadow <wolfgang.schadow@t-online.de>
2022-06-11 00:14:50 +0200

    updated www files; corrected indentation in ActivateAutoload, updated config-override ; config-override.g file generated in response to M500 at 2022-06-10 23:56 ; This is a system-generated file - do not edit ; Heater model parameters M307 H0 R0.264 K0.232:0.000 D17.11 E1.00 S1.00 B0 M307 H1 R2.429 K0.714:0.000 D5.50 E1.00 S1.00 B0 V0.0 ; Z probe parameters G31 K0 P1000 X23.0 Y5.0 Z0.70 ; Probed tool offsets G10 P0 X0.00 Y0.00 Z0.00 0.00 0.00 0.00 0.00 0.00 0.00 0.00 ; Workplace coordinates G10 L2 P1 X0.00 Y0.00 Z0.00 G10 L2 P2 X0.00 Y0.00 Z0.00 G10 L2 P3 X0.00 Y0.00 Z0.00 G10 L2 P4 X0.00 Y0.00 Z0.00 G10 L2 P5 X0.00 Y0.00 Z0.00 G10 L2 P6 X0.00 Y0.00 Z0.00 G10 L2 P7 X0.00 Y0.00 Z0.00 G10 L2 P8 X0.00 Y0.00 Z0.00 G10 L2 P9 X1.00 Y1.00 Z0.00

--- Configuration/sys/variants/Caribou420-HP.sh
+++ Configuration/sys/variants/Caribou420-HP.sh
@@ -82,8 +82,7 @@ sed -i "
 ;\\
 M308 S1 P\"e0temp\" Y\"thermistor\" T500000 B4723 C1.19622e-7 A\"Nozzle\"   ; SE configure sensor 0 as thermistor on pin e0temp\\
 ;\\
-M950 H1 C\"e0heat\" T1                                   ; create nozzle heater output on e0heat and map it to sensor 1
-\\
+M950 H1 C\"e0heat\" T1                                   ; create nozzle heater output on e0heat and map it to sensor 1\\
 M307 H1 B0 S1.00                                       ; disable bang-bang mode for heater  and set PWM limit\\
 M143 H1 S365                                           ; set temperature limit for heater 1 to 365°C
 };
