11618f97b7b7e7776a00098243702e3ae25beac2
wschadow <wolfgang.schadow@t-online.de>
2023-01-28 15:19:16 +0100

    updates replacesments for FilamentSensorStatus and RunOutOff

--- Configuration/sys/variants/Caribou220-HBLL.sh
+++ Configuration/sys/variants/Caribou220-HBLL.sh
@@ -76,16 +76,17 @@ if [ "$DUETBOARD" = "DUET2" ]; then
 # Duet 2
 sed -i "
 {/#CARIBOU_DISABLE_FILAMENT_SENSOR/ c\
-\       M591 D0 P0 C\"e0stop\" S1                                         ; disable filament runout sensor
+\    M591 D0 P0 C\"e0stop\" S1                                        ; disable filament runout sensor
 };
 " $SysOutputPath/00-Functions/RunOutOff
 else
 sed -i "
 {/#CARIBOU_DISABLE_FILAMENT_SENSOR/ c\
-\       M591 D0 P0 C\"io2.in\" S0                                         ; disable filament runout sensor
+\    M591 D0 P0 C\"io2.in\" S0                                        ; disable filament runout sensor
 };
 " $SysOutputPath/00-Functions/RunOutOff
 fi
+
 
 if [ "$DUETBOARD" = "DUET2" ]; then
 # Duet 2
