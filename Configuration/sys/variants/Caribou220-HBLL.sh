2fd015815f650a02eac0ada0f963e4bb019d1f8d
wschadow <wolfgang.schadow@t-online.de>
2023-02-02 15:24:23 +0100

    corrects PIN for filament sensor für Duet3Mini5+

--- Configuration/sys/variants/Caribou220-HBLL.sh
+++ Configuration/sys/variants/Caribou220-HBLL.sh
@@ -61,15 +61,15 @@ if [ "$DUETBOARD" = "DUET2" ]; then
 # Duet 2
 sed -i "
 {/#CARIBOU_TRIGGER/ c\
-\            M950 J0 C\"e0stop\"                                          ; input 0 filament sensor
+\       M950 J0 C\"e0stop\"                                               ; input 0 filament sensor
 };
-" $SysOutputPath/00-Functions/FilamentSensorStatus
+" $SysOutputPath/00-Functions/ChangeFilament
 else
 sed -i "
 {/#CARIBOU_TRIGGER/ c\
-\            M950 J0 C\"io2.in\"                                          ; input 0 filament sensor
+\       M950 J0 C\"io2.in\"                                               ; input 0 filament sensor
 };
-" $SysOutputPath/00-Functions/FilamentSensorStatus
+" $SysOutputPath/00-Functions/ChangeFilament
 fi
 
 if [ "$DUETBOARD" = "DUET2" ]; then
@@ -85,6 +85,36 @@ sed -i "
 \    M591 D0 P0 C\"io2.in\" S0                                        ; disable filament runout sensor
 };
 " $SysOutputPath/00-Functions/RunOutOff
+fi
+
+if [ "$DUETBOARD" = "DUET2" ]; then
+# Duet 2
+sed -i "
+{/#CARIBOU_TRIGGER/ c\
+\            M950 J0 C\"e0stop\"                                          ; input 0 filament sensor
+};
+" $SysOutputPath/00-Functions/FilamentSensorStatus
+else
+sed -i "
+{/#CARIBOU_TRIGGER/ c\
+\            M950 J0 C\"io2.in\"                                          ; input 0 filament sensor
+};
+" $SysOutputPath/00-Functions/FilamentSensorStatus
+fi
+
+if [ "$DUETBOARD" = "DUET2" ]; then
+# Duet 2
+sed -i "
+{/#CARIBOU_ENABLE_FILAMENT_SENSOR/ c\
+M591 D0 P2 C\"e0stop\" S1                                                ; filament runout sensor
+};
+" $SysOutputPath/00-Functions/RunOutOn
+else
+sed -i "
+{/#CARIBOU_ENABLE_FILAMENT_SENSOR/ c\
+M591 D0 P2 C\"io2.in\" S1                                                ; filament runout sensor
+};
+" $SysOutputPath/00-Functions/RunOutOn
 fi
 
 
