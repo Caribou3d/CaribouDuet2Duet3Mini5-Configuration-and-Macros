a479bbd403c5a0c68334af43a1189cefcb20490d
wschadow <wolfgang.schadow@t-online.de>
2023-11-06 00:39:08 +0100

    updates formatting, removes dead code

--- Configuration/sys/variants/Caribou320-LGXM09-NP.sh
+++ Configuration/sys/variants/Caribou320-LGXM09-NP.sh
@@ -490,14 +490,4 @@ sed "
 {s/#CARIBOU_FINALUNLOAD/$CARIBOU_FINALUNLOAD/g}
 " < $MacrosDir/01-Filament_Handling/01-Unload_Filament > $MacroOutputPath/01-Filament_Handling/01-Unload_Filament
 
-# create Change_Filament
-#
-sed "
-{s/#CARIBOU_VARIANT/$CARIBOU_VARIANT/};
-{s/#CARIBOU_MINEXTRUDETEMP/$CARIBOU_MINEXTRUDETEMP/};
-{s/#CARIBOU_MINRETRACTTEMP/$CARIBOU_MINRETRACTTEMP/};
-{s/#CARIBOU_INITIALLOAD/$CARIBOU_INITIALLOAD/g};
-{s/#CARIBOU_FINALUNLOAD/$CARIBOU_FINALUNLOAD/g}
-" < $MacrosDir/01-Filament_Handling/03-Change_Filament > $MacroOutputPath/01-Filament_Handling/03-Change_Filament
-
 # =========================================================================================================
